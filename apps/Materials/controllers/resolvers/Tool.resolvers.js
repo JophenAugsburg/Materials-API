995ef5e49101988bf093f29cfa17646d3d8069dc
Joseph Hentges <joeypeterhentges@gmail.com>
2019-11-24 01:00:37 -0600

    split apart resolvers into their own files - querys and mutations file

--- apps/Materials/controllers/resolvers/Tool.resolvers.js
+++ apps/Materials/controllers/resolvers/Tool.resolvers.js
@@ -1,70 +1,7 @@
-const { updateLogs } = require('../../../tools');
-const { Material } = require('../../models/Material');
+const toolQueryResolvers = require('./Tool.Query.resolvers');
+const toolMutationResolvers = require('./Tool.Mutation.resolvers');
 
-const getMaterials = () => Material.find();
-
-const getMaterialById = id => Material.findById(id);
-
-const getMaterialByName = name => Material.findOne({ name });
-
-const getMaterialByType = type => Material.findOne({ type });
-
-const createMaterial = async (name, userId, type, quantity) => {
-  const dateAdded = new Date();
-  const material = new Material({
-    createdBy: userId,
-    name,
-    type,
-    dateAdded,
-    quantity,
-    quantityHistory: [
-      {
-        userId,
-        type: "add",
-        quanitity,
-        date: dateAdded,
-      },
-    ],
-    checkoutList: [],
-    logs: [],
-  });
-  await material.save();
-  await updateLogs(material._id, 'Material', 'material', 1, name);
-  return material;
+module.exports.materialResolvers = {
+  Query: toolQueryResolvers,
+  Mutation: toolMutationResolvers
 };
-
-updateMaterial = async (id, userId, updateVariable, updateValue) => {
-
-}
-
-deleteMaterial = async (id) => Material.findByIdAndDelete(id);
-
-checkoutMaterial = async (id, userId, quantity) => {
-
-}
-
-// Functions condenced to be exported
-const materialResolvers = {
-  Query: {
-    getMaterials: () => getMaterials(),
-    getMaterialById: ({ id }) => getMaterialById(id),
-    getMaterialByName: ({ name }) => getMaterialByName(name),
-    getMaterialByType: ({ type }) => getMaterialByType(type),
-  },
-  Mutation: {
-    createMaterial: ({
-      name, userId, type, quantity
-    }) => createMaterial(name, userId, type, quantity),
-    updateMaterial: ({
-      id, userId, updateVariable, updateValue
-    }) => updateMaterial(id, userId, updateVariable, updateValue),
-    deleteMaterial: ({
-      id
-    }) => deleteMaterial(id),
-    checkoutMaterial: ({
-      id, userId, quantity
-    }) => checkoutMaterial(id, userId, quantity),
-  }
-};
-
-module.exports.materialResolvers = materialResolvers;
