ab42612bcfe02a5f66ee772a3b0efb8c3aa4ee5c
Joseph Hentges <hentgesj@augsburg.edu>
2019-12-05 12:55:01 -0600

    Update check-key.js

--- main/controllers/api-tools/check-key.js
+++ main/controllers/api-tools/check-key.js
@@ -1,5 +1,6 @@
 // Check the key
 module.exports.checkKey = async (req, res, next) => {
+  console.log(req.body);
   if (req.body.key != process.env.API_KEY) {
     console.log('invalid key', req.body.key, 'tried');
     res.send('invalid key');
